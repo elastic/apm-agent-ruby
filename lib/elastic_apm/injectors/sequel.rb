523993c135e7072bb739a8fdc25824bd3e2928ee
Mikkel Malmberg <mikkel@brnbw.com>
2017-11-22 15:14:02 +0100

    Add Sequel support

---
+++ lib/elastic_apm/injectors/sequel.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'elastic_apm/sql_summarizer'
+
+module ElasticAPM
+  # @api private
+  module Injectors
+    # @api private
+    class SequelInjector
+      TYPE = 'db.sequel.sql'
+
+      def self.summarizer
+        @summarizer ||= SqlSummarizer.new
+      end
+
+      def install
+        require 'sequel/database/logging'
+
+        log_method =
+          if ::Sequel::Database.method_defined?(:log_connection_yield)
+            'log_connection_yield'
+          else
+            'log_yield'
+          end
+
+        ::Sequel::Database.class_eval <<-RUBY
+          alias #{log_method}_without_apm #{log_method}
+
+          def #{log_method}(sql, *args, &block)
+            unless ElasticAPM.started? && ElasticAPM.agent.current_transaction
+              return #{log_method}_without_apm(sql, *args, &block)
+            end
+
+            #{log_method}_without_apm(sql, *args) do
+              summarizer = ElasticAPM::Injectors::SequelInjector.summarizer
+              name = summarizer.summarize sql
+
+              ElasticAPM.trace(name, TYPE, sql: sql, &block)
+            end
+          end
+        RUBY
+      end
+    end
+
+    register 'Sequel', 'sequel', SequelInjector.new
+  end
+end
