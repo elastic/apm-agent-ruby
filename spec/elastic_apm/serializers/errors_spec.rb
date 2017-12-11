2587ae0db4e25f7cf1b94d795d55c6afb0e56ff3
Mikkel Malmberg <mikkel@elastic.co>
2017-12-11 12:42:47 +0100

    Let serializers generate single payloads

--- spec/elastic_apm/serializers/errors_spec.rb
+++ spec/elastic_apm/serializers/errors_spec.rb
@@ -4,26 +4,34 @@ module ElasticAPM
   module Serializers
     RSpec.describe Errors do
       let(:config) { Config.new }
-      subject { Errors.new config }
-      before { allow(SecureRandom).to receive(:uuid) { '_RANDOM' } }
 
-      describe '#build' do
-        it 'builds an error from exception', :mock_time do
-          error = ErrorBuilder.new(config).build(actual_exception)
+      let(:builder) { Errors.new config }
+      before do
+        @mock_uuid = SecureRandom.uuid
+        allow(SecureRandom).to receive(:uuid) { @mock_uuid }
+      end
 
-          result = subject.build([error]).dig(:errors, 0)
-          expect(result[:id]).to eq '_RANDOM'
-          expect(result[:culprit]).to eq '/'
-          expect(result[:timestamp]).to eq(Time.utc(1992, 1, 1).iso8601)
-          expect(result[:exception]).to eq(
-            message: 'ZeroDivisionError: divided by 0',
-            type: 'ZeroDivisionError',
-            module: '',
-            code: nil,
-            attributes: nil,
-            stacktrace: error.exception.stacktrace.to_h, # so lazy
-            unhandled: false
-          )
+      describe '#build', :mock_time do
+        context 'an error with exception' do
+          let(:error) { ErrorBuilder.new(config).build(actual_exception) }
+          subject { builder.build(error) }
+
+          it 'builds' do
+            should eq(
+              id: @mock_uuid,
+              culprit: '/',
+              timestamp: Time.utc(1992, 1, 1).iso8601,
+              exception: {
+                message: 'ZeroDivisionError: divided by 0',
+                type: 'ZeroDivisionError',
+                module: '',
+                code: nil,
+                attributes: nil,
+                stacktrace: error.exception.stacktrace.to_h, # so lazy
+                unhandled: false
+              }
+            )
+          end
         end
       end
     end
