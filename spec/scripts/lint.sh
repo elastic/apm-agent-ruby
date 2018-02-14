08e394b33402c8091580052e2ed05bc9b7161283
Mikkel Malmberg <mikkel@elastic.co>
2018-02-14 10:11:21 +0100

    Make lint and docs scripts do nothing

--- spec/scripts/lint.sh
+++ spec/scripts/lint.sh
@@ -1,17 +1,2 @@
 #!/usr/bin/env bash
-set -ex
-
-local_vendor_path="$HOME/.cache/ruby-vendor"
-container_vendor_path="/tmp/vendor/2.4.1"
-
-mkdir -p $local_vendor_path
-
-cd spec
-
-docker build --pull --build-arg RUBY_VERSION=ruby:2.4.1 -t lint .
-docker run \
-  -e LOCAL_USER_ID=$UID \
-  -v "$local_vendor_path:$container_vendor_path" \
-  -v "$(dirname $(pwd))":/app \
-  --rm lint \
-  /bin/bash -c "bundle install --path $container_vendor_path && rubocop"
+echo 'noop'
