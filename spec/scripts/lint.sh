3a62406143640303d3dd8b8c48a6b7e3d4c3bd6f
Benjamin Wohlwend <beni@elastic.co>
2018-02-14 17:12:05 +0100

    tag docker containers to avoid name clashes

--- spec/scripts/lint.sh
+++ spec/scripts/lint.sh
@@ -8,10 +8,10 @@ mkdir -p $local_vendor_path
 
 cd spec
 
-docker build --pull --build-arg RUBY_VERSION=ruby:2.4.1 -t lint .
+docker build --pull --force-rm --build-arg RUBY_VERSION=ruby:2.4.1 -t lint:ruby .
 docker run \
   -e LOCAL_USER_ID=$UID \
   -v "$local_vendor_path:$container_vendor_path" \
   -v "$(dirname $(pwd))":/app \
-  --rm lint \
+  --rm lint:ruby \
   /bin/bash -c "bundle install --path $container_vendor_path && rubocop"
