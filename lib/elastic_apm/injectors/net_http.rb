f7e0c594fb2848a716bcacf6e7dc5c7bf9b800ef
Mikkel Malmberg <mikkel@elastic.co>
2018-04-13 14:46:31 +0200

    Remove unneeded variables

--- lib/elastic_apm/injectors/net_http.rb
+++ lib/elastic_apm/injectors/net_http.rb
@@ -5,7 +5,7 @@ module ElasticAPM
   module Injectors
     # @api private
     class NetHTTPInjector
-      # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+      # rubocop:disable Metrics/MethodLength
       def install
         Net::HTTP.class_eval do
           alias request_without_apm request
@@ -15,14 +15,10 @@ module ElasticAPM
               return request_without_apm(req, body, &block)
             end
 
-            host, port = req['host'] && req['host'].split(':')
+            host, = req['host'] && req['host'].split(':')
             method = req.method
-            path = req.path
-            scheme = use_ssl? ? 'https' : 'http'
 
-            # inside a session
             host ||= address
-            port ||= 80
 
             name = "#{method} #{host}"
             type = "ext.net_http.#{method}"
@@ -33,7 +29,7 @@ module ElasticAPM
           end
         end
       end
-      # rubocop:enable Metrics/AbcSize, Metrics/MethodLength
+      # rubocop:enable Metrics/MethodLength
     end
 
     register 'Net::HTTP', 'net/http', NetHTTPInjector.new
