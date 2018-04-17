0fcbd67d6f05c343a0fbd2a3e228673bcd465fcc
Mikkel Malmberg <mikkel@elastic.co>
2018-04-17 11:43:41 +0200

    Add Mongo instance to specs

--- spec/scripts/spec.sh
+++ spec/scripts/spec.sh
@@ -1,5 +1,11 @@
 #!/usr/bin/env bash
-set -e
+set -ex
+
+function cleanup {
+  docker-compose down -v
+}
+
+trap cleanup EXIT
 
 if [ $# -lt 2 ]; then
   echo "Arguments missing"
@@ -13,12 +19,16 @@ mkdir -p $local_vendor_path
 
 cd spec
 
-docker build --pull --force-rm --build-arg RUBY_VERSION=${1/-/:} -t apm-agent-ruby:${1} .
-docker run \
+RUBY_IMAGE=${1/-/:}
+
+docker-compose up -d mongodb
+
+docker build --pull --force-rm --build-arg RUBY_IMAGE=$RUBY_IMAGE -t apm-agent-ruby:$1 .
+RUBY_VERSION=$1 docker-compose run \
   -e LOCAL_USER_ID=$UID \
   -e FRAMEWORK=$2 \
   -e INCLUDE_SCHEMA_SPECS=1 \
   -v "$local_vendor_path:$container_vendor_path" \
   -v "$(dirname $(pwd))":/app \
-  --rm apm-agent-ruby:${1} \
+  --rm ruby_rspec \
   /bin/bash -c "bundle install --path $container_vendor_path && timeout -s9 5m bundle exec rspec ${3:-spec}"
