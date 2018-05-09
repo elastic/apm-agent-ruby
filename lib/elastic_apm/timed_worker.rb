20ebf7757bf3887a34a627ff3cadad3211bf1061
Mikkel Malmberg <mikkel@elastic.co>
2018-05-09 14:33:53 +0200

    Update last sent timestamp after posting transactions

--- lib/elastic_apm/timed_worker.rb
+++ lib/elastic_apm/timed_worker.rb
@@ -84,6 +84,7 @@ module ElasticAPM
       @adapter.post('/v1/errors', payload)
     end
 
+    # rubocop:disable Metrics/MethodLength
     def collect_and_send_transactions
       puts "WANNA SEND"
       return if pending_transactions.empty?
@@ -100,7 +101,10 @@ module ElasticAPM
         debug e.backtrace.join("\n")
         nil
       end
+
+      @last_sent_transactions = Time.now
     end
+    # rubocop:enable Metrics/MethodLength
 
     def collect_batched_transactions
       batch = []
