104529a4840226ae75b3501e1cee06d51025ab84
Chris Heald <cheald@gmail.com>
2018-07-06 02:00:53 -0700

    Minor changes to the optional loading pattern for HttpRbAdapter

--- lib/elastic_apm/http_adapters/http_rb_adapter.rb
+++ lib/elastic_apm/http_adapters/http_rb_adapter.rb
@@ -1,40 +1,42 @@
 require 'elastic_apm/http_adapters/abstract_adapter'
+enable = false
 begin
   require 'http'
+  enable = true
+rescue LoadError
+end
 
-  module ElasticAPM
-    module HttpAdapters
-      # @api private
-      class HttpRbAdapter < AbstractHttpAdapter
-        def perform(uri, data, headers)
-          return DISABLED if @config.disable_send?
+module ElasticAPM
+  module HttpAdapters
+    # @api private
+    class HttpRbAdapter < AbstractHttpAdapter
+      def perform(uri, data, headers)
+        return DISABLED if @config.disable_send?
 
-          req = client(uri).headers(headers)
-          if @config.http_compression &&
-            data.bytesize > @config.compression_minimum_size
-            req = req.use(auto_deflate: { method: :deflate })
-          end
-          Response.new req.post(uri, body: data, ssl_context: get_context)
+        req = client(uri).headers(headers)
+        if @config.http_compression &&
+          data.bytesize > @config.compression_minimum_size
+          req = req.use(auto_deflate: { method: :deflate })
         end
+        Response.new req.post(uri, body: data, ssl_context: get_context)
+      end
 
-        private
+      private
 
-        def client(uri)
-          @client ||= begin
-            keepalive = @config.http_adapter_options[:keepalive]
-            keepalive ? ::HTTP.persistent(uri) : ::HTTP
-          end
+      def client(uri)
+        @client ||= begin
+          keepalive = @config.http_adapter_options[:keepalive]
+          keepalive ? ::HTTP.persistent(uri) : ::HTTP
         end
+      end
 
-        def get_context
-          ctx = OpenSSL::SSL::SSLContext.new
-          if @config.use_ssl? && @config.verify_server_cert?
-            ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
-          end
-          ctx
+      def get_context
+        ctx = OpenSSL::SSL::SSLContext.new
+        if @config.use_ssl? && @config.verify_server_cert?
+          ctx.verify_mode = OpenSSL::SSL::VERIFY_NONE
         end
+        ctx
       end
     end
   end
-rescue LoadError
-end
+end if enable
