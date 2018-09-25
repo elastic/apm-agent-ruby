bc96462040c7d5da0218fb88eedf70c0a06f69c7
Mikkel Malmberg <mikkel@elastic.co>
2018-09-25 12:56:39 +0200

    Only test Normalizers when AS::Notifications exist

--- spec/elastic_apm/normalizers_spec.rb
+++ spec/elastic_apm/normalizers_spec.rb
@@ -1,19 +1,28 @@
 # frozen_string_literal: true
 
-require 'spec_helper'
-require 'elastic_apm/subscriber'
+enable = false
+begin
+  require 'active_support/notifications'
+  enable = true
+rescue LoadError
+  puts '[INFO] Skipping Normalizers spec'
+end
 
-module ElasticAPM
-  RSpec.describe Normalizers do
-    describe 'registration:' do
-      it 'allows a normalizer to register itself' do
-        class TestNormalizer < Normalizers::Normalizer
-          register 'something'
+if enable
+  require 'elastic_apm/subscriber'
+
+  module ElasticAPM
+    RSpec.describe Normalizers do
+      describe 'registration:' do
+        it 'allows a normalizer to register itself' do
+          class TestNormalizer < Normalizers::Normalizer
+            register 'something'
+          end
+
+          built = Normalizers.build nil
+          expect(built.for('something')).to be_a TestNormalizer
+          expect(built.keys).to include 'something'
         end
-
-        built = Normalizers.build nil
-        expect(built.for('something')).to be_a TestNormalizer
-        expect(built.keys).to include 'something'
       end
     end
   end
