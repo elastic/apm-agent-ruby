c23334a292d62e32b1b3434b8f272acadb6633de
Mikkel Malmberg <mikkel@elastic.co>
2018-11-14 17:29:48 +0100

    Dont parse JSON from APM Server errors

--- lib/elastic_apm/transport/connection.rb
+++ lib/elastic_apm/transport/connection.rb
@@ -115,8 +115,7 @@ module ElasticAPM
           if resp&.status == 202
             debug 'APM Server responded with status 202'
           elsif resp
-            error "APM Server reponded with an error:\n%p",
-              JSON.parse(resp.body.to_s)
+            error "APM Server reponded with an error:\n%p", resp.body.to_s
           end
 
           resp
