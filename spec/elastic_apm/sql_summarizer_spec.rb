00de4cbae49fc37c41f28156149e7ff875a532da
Mikkel Malmberg <mikkel@elastic.co>
2019-01-03 21:58:15 +0100

    Allow underscores in table names when summarizing sql

--- spec/elastic_apm/sql_summarizer_spec.rb
+++ spec/elastic_apm/sql_summarizer_spec.rb
@@ -14,6 +14,11 @@ module ElasticAPM
       expect(result).to eq('SELECT FROM table')
     end
 
+    it 'summarizes selects from table with underscore' do
+      result = subject.summarize('SELECT * FROM my_table')
+      expect(result).to eq('SELECT FROM my_table')
+    end
+
     it 'simplifies advanced selects' do
       result = subject.summarize("select months.month, count(created_at) from (select DATE '2017-06-09'+(interval '1' month * generate_series(0,11)) as month, DATE '2017-06-10'+(interval '1' month * generate_series(0,11)) as next) months left outer join subscriptions on created_at < month and (soft_destroyed_at IS NULL or soft_destroyed_at >= next) and (suspended_at IS NULL OR suspended_at >= next) group by month order by month desc") # rubocop:disable Metrics/LineLength
       expect(result).to eq('SQL')
