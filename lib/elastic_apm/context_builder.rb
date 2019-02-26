77cd755b31f2383629997eb4c02329eca2d94279
Mikkel Malmberg <mikkel@elastic.co>
2019-02-26 12:00:59 +0100

    Force encode captured bodies to utf-8

--- lib/elastic_apm/context_builder.rb
+++ lib/elastic_apm/context_builder.rb
@@ -51,7 +51,7 @@ module ElasticAPM
       else
         body = req.body.read
         req.body.rewind
-        body.byteslice(0, MAX_BODY_LENGTH)
+        body.byteslice(0, MAX_BODY_LENGTH).force_encoding('utf-8')
       end
     end
 
