ef5b3ef307233daa9ad2ce7245f86fff89a93dae
simitt <silvia.mitter@elastic.co>
2019-05-01 13:06:58 +0200

    Revert "Init one worker thread per process (#4)"
    This reverts commit 735c617992d4a0a885394881d649b1a0923c6ba7.

--- lib/elastic_apm/transport/base.rb
+++ lib/elastic_apm/transport/base.rb
@@ -20,23 +20,22 @@ module ElasticAPM
         @filters = Filters.new(config)
 
         @stopped = Mutex.new
-        @workers = Concurrent::Hash.new
+        @pool = Concurrent::FixedThreadPool.new(config.pool_size)
+        @workers = Concurrent::Array.new
+        @supervisor = init_supervisor
       end
 
       def start
         debug 'Starting Transport'
-        boot_worker
+        ensure_worker_count
+        supervisor.execute
       end
 
       def stop
         debug 'Stopping Transport'
         @stopped.try_lock
-        send_stop_message
-
-        @workers.each do |_pid, t|
-          t.join(5)
-        end
-        @workers.clear
+        supervisor.shutdown
+        stop_workers
       end
 
       def submit(resource)
@@ -44,8 +43,6 @@ module ElasticAPM
           warn 'Transport stopping, new events not accepted…'
           return false
         end
-
-        boot_worker
         queue.push(resource, true)
       rescue ThreadError
         warn 'Queue is full (%i items), skipping…', config.api_buffer_size
@@ -64,28 +61,65 @@ module ElasticAPM
       private
 
       attr_reader :config, :filters, :pool, :workers, :supervisor
-      def boot_worker
-        return if @workers[Process.pid] && @workers[Process.pid].alive?
-        debug 'Booting worker...'
 
-        @workers[Process.pid] = Thread.new do
-          Worker.new(
-            config,
-            queue,
-            serializers: @serializers,
-            filters: @filters
-          ).work_forever
-        end
+      def missing_worker
+        config.pool_size - workers.length
       end
 
-      def send_stop_message
-        queue.push(Worker::StopMessage.new, true)
+      def ensure_worker_count
+        missing_worker.times { boot_worker }
+      end
+
+      # rubocop:disable Metrics/MethodLength
+      def boot_worker
+        return if missing_worker <= 0
+
+        debug 'Booting worker...'
+        worker = Worker.new(
+          config,
+          queue,
+          serializers: @serializers,
+          filters: @filters
+        )
+        @workers.push worker
+
+        @pool.post do
+          worker.work_forever
+          @workers.delete(worker)
+        end
+      end
+      # rubocop:enable Metrics/MethodLength
+
+      def stop_workers
+        return unless @pool.running?
+
+        debug 'Stopping workers'
+        send_stop_messages
+
+        debug 'Shutting down pool'
+        @pool.shutdown
+
+        return if @pool.wait_for_termination(5)
+
+        warn "Worker pool didn't close in 5 secs, killing ..."
+        @pool.kill
+      end
+
+      def send_stop_messages
+        config.pool_size.times { queue.push(Worker::StopMessage.new, true) }
       rescue ThreadError
         warn 'Cannot push stop messages to worker queue as it is full'
       end
 
       def stopped?
         @stopped.locked?
+      end
+
+      def init_supervisor
+        @supervisor = Concurrent::TimerTask.new(execution_interval: 2,
+                                                timeout_interval: 1) do
+          ensure_worker_count unless stopped?
+        end
       end
     end
   end
