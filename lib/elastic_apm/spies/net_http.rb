ba6b506518dfe8580b2be02013006596ce66809e
Mikkel Malmberg <mikkel@brnbw.com>
2019-05-01 13:45:39 +0200

    Revert "net-http spy: use Module.prepend instead of alias (#382)"
    This reverts commit 890d93a9d95e2c620019cde09f2659deececa498.

--- lib/elastic_apm/spies/net_http.rb
+++ lib/elastic_apm/spies/net_http.rb
@@ -28,33 +28,32 @@ module ElasticAPM
         end
       end
 
-      module Overrides
-        # @api private
-        def request(req, body = nil)
-          return super if ElasticAPM::Spies::NetHTTPSpy.disabled?
-
-          transaction = ElasticAPM.current_transaction
-          return super unless transaction
-
-          host, = req['host'] && req['host'].split(':')
-          method = req.method
-
-          host ||= address
-
-          name = "#{method} #{host}"
-          type = "ext.net_http.#{method}"
-
-          ElasticAPM.with_span name, type do |span|
-            trace_context = span&.trace_context || transaction.trace_context
-            req['Elastic-Apm-Traceparent'] = trace_context.to_header
-            super
-          end
-        end
-      end
-
       def install
         Net::HTTP.class_eval do
-          prepend Overrides
+          alias request_without_apm request
+
+          def request(req, body = nil, &block)
+            unless (transaction = ElasticAPM.current_transaction)
+              return request_without_apm(req, body, &block)
+            end
+            if ElasticAPM::Spies::NetHTTPSpy.disabled?
+              return request_without_apm(req, body, &block)
+            end
+
+            host, = req['host'] && req['host'].split(':')
+            method = req.method
+
+            host ||= address
+
+            name = "#{method} #{host}"
+            type = "ext.net_http.#{method}"
+
+            ElasticAPM.with_span name, type do |span|
+              trace_context = span&.trace_context || transaction.trace_context
+              req['Elastic-Apm-Traceparent'] = trace_context.to_header
+              request_without_apm(req, body, &block)
+            end
+          end
         end
       end
       # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
