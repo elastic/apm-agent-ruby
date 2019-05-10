b26c152bfd10ee908e992d81dfb282064b78f8a6
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-10 09:18:02 +0100

    Lint shell with shellcheck

--- spec/scripts/benchmarks.sh
+++ spec/scripts/benchmarks.sh
@@ -7,27 +7,28 @@ if [ $# -lt 1 ]; then
 fi
 
 RUBY_IMAGE=${1}
-VERSION=$(echo $RUBY_IMAGE | cut -d":" -f2)
+VERSION=$(echo "${RUBY_IMAGE}" | cut -d":" -f2)
 
 ## Transform the versions like:
 ##  - docker.elastic.co/observability-ci/jruby:9.2-12-jdk to jruby-9.2-12-jdk
 ##  - jruby:9.1 to jruby-9.1
-TRANSFORMED_VERSION=$(basename $RUBY_IMAGE | sed "s#:#-#g")
+TRANSFORMED_VERSION=$(basename "${RUBY_IMAGE}" | sed "s#:#-#g")
 
 local_vendor_path="$HOME/.cache/ruby-vendor"
 container_vendor_path="/tmp/vendor/${TRANSFORMED_VERSION/ruby-/}"
+CURRENT_PATH=$(dirname "$(pwd)")
 
-mkdir -p $local_vendor_path
+mkdir -p "${local_vendor_path}"
 
 cd spec
 
-docker build --pull --force-rm --build-arg RUBY_IMAGE=$RUBY_IMAGE -t apm-agent-ruby:${VERSION} .
+docker build --pull --force-rm --build-arg "RUBY_IMAGE=${RUBY_IMAGE}" -t "apm-agent-ruby:${VERSION}" .
 RUBY_VERSION=${VERSION} docker-compose run \
   --user $UID \
   -e HOME=/app \
   -w /app \
   -e LOCAL_USER_ID=$UID \
   -v "$local_vendor_path:$container_vendor_path" \
-  -v "$(dirname $(pwd))":/app \
+  -v "${CURRENT_PATH}:/app" \
   --rm ruby_rspec \
   /bin/bash -c "bundle install --path $container_vendor_path && bench/benchmark.rb 2> /dev/null | bench/report.rb > benchmark-${TRANSFORMED_VERSION}.bulk"
