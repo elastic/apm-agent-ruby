af0bcb9d7d322d7eb622314f121d3b5fed41dd8b
Victor Martinez <VictorMartinezRubio@gmail.com>
2019-05-10 09:30:51 +0100

    Fix shellcheck lint defects

--- spec/scripts/spec.sh
+++ spec/scripts/spec.sh
@@ -6,19 +6,21 @@ if [ $# -lt 2 ]; then
   exit 2
 fi
 
-cd spec
-
 RUBY_IMAGE=${1}
-VERSION=$(echo $1 | cut -d":" -f2)
+FRAMEWORK=${2}
+TEST=${3:-spec}
+VERSION=$(echo "${RUBY_IMAGE}" | cut -d":" -f2)
+
+cd spec
 
 docker-compose up -d mongodb
 
-docker build --pull --build-arg RUBY_IMAGE=$RUBY_IMAGE -t apm-agent-ruby:${VERSION} .
+docker build --pull --build-arg "RUBY_IMAGE=${RUBY_IMAGE}" -t "apm-agent-ruby:${VERSION}" .
 RUBY_VERSION=${VERSION} docker-compose run \
-  -e FRAMEWORK=$2 \
+  -e FRAMEWORK="${FRAMEWORK}" \
   -e INCLUDE_SCHEMA_SPECS=1 \
-  -v "$(dirname $(pwd))":/app \
+  -v "$(dirname "$(pwd)"):/app" \
   --rm ruby_rspec \
   /bin/bash -c "\
     bundle install && \
-    timeout -s9 5m bundle exec rspec -f progress -f JUnit -o spec/ruby-agent-junit.xml ${3:-spec}"
+    timeout -s9 5m bundle exec rspec -f progress -f JUnit -o spec/ruby-agent-junit.xml ${TEST}"
