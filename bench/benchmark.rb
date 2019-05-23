6966fb13e80533ca18cec9d4b4c0f49e6f10bf99
Mikkel Malmberg <mikkel@elastic.co>
2019-05-23 14:30:07 +0200

    Update benchmark script (#414)

--- bench/benchmark.rb
+++ bench/benchmark.rb
@@ -1,64 +1,31 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require 'bundler/setup'
-
 require 'benchmark'
 include Benchmark
-
 require 'rack/test'
 
 require './bench/app'
-include App::Helpers
 
-def perform(app, count: 1000)
-  app.start
-
-  transactions = count.times.map do |i|
-    ElasticAPM.transaction "Transaction##{i}",
-      context: ElasticAPM.build_context(app.mock_env) do
-      ElasticAPM.span('Number one') { 'ok 1' }
-      ElasticAPM.span('Number two') { 'ok 2' }
-      ElasticAPM.span('Number three') { 'ok 3' }
-    end
-  end
-
-  # app.serializer.build_all(transactions)
-
-  app.stop
+def app
+  App
 end
 
-def avg(benchmarks)
-  [benchmarks.reduce(Tms.new(0), &:+) / benchmarks.length]
-end
+include Rack::Test::Methods
 
-def banner(text)
-  puts "=== #{text}"
-end
-
-def do_bench(transaction_count: 10, **config)
-  puts "Count: #{transaction_count} transactions... \n \n"
-
-  bench = Benchmark.benchmark(CAPTION, 7, FORMAT, 'avg:') do |x|
-    benchmarks =
-      with_app(config) do |app|
-        # warm-up
-        puts "1 run of warm-up"
-        perform(app, count: transaction_count)
-
-        5.times.map do |i|
-          x.report("run[#{i}]") { perform(app, count: transaction_count) }
-        end
-      end
-
-    avg(benchmarks)
+def perform
+  10_000.times do
+    get '/'
   end
 end
 
-transaction_count = Integer(ARGV.shift || 100_000)
+bench = Benchmark.benchmark(CAPTION, 15, FORMAT) do |b|
+  perform # warm up
 
-banner 'Default settings'
-do_bench transaction_count: transaction_count
+  b.report('with agent:') { perform }
 
-banner 'With transaction_sample_rate = 0'
-do_bench(transaction_count: transaction_count, transaction_sample_rate: 0)
+  ElasticAPM.stop
+  perform # warm up
+
+  b.report('without agent:') { perform }
+end
--- bench/report.rb
+++ bench/report.rb
@@ -1,46 +1,40 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-Encoding.default_external = 'utf-8'
+# Encoding.default_external = 'utf-8'
 
 require 'time'
-require 'bundler/setup'
 require 'json'
 
 input = STDIN.read.split("\n")
-STDERR.puts input
-
-titles = input.grep(/^===/).map { |t| t.gsub(/^=== /, '') }
-counts = input.grep(/^Count: /).map { |a| a.gsub(/^Count: /, '').to_i }
-averages = input.grep(/^avg/).map { |a| a.match(/\((.+)\)/)[1].to_f }
 
 git_sha, git_msg = `git log -n 1 --pretty="format:%H|||%s"`.split('|||')
 git_date = `git log -n 1 --pretty="format:%ai"`
 platform = Gem::Platform.local
 
-payloads = titles.zip(averages, counts).map do |(title, avg, count)|
-  return nil unless avg
+results =
+  input
+  .grep(/^with/)
+  .map do |line|
+    title = line.match(/^(.*):/) { |m| m[1] }
+    user, system, total, real = line.scan(/[0-9\.]+/).map(&:to_f)
+    {
+      title: title,
+      user: user,
+      system: system,
+      total: total,
+      real: real,
+      executed_at: Time.new.iso8601,
+      'git.commit' => git_sha,
+      'git.date' => String(git_date).strip != '' && Time.parse(git_date).iso8601,
+      'git.subject' => git_msg,
+      hostname: `hostname`.chomp,
+      engine: RUBY_ENGINE,
+      arch: platform.cpu,
+      os: platform.os,
+      ruby_version: "#{RUBY_ENGINE == 'jruby' ? 'j' : ''}#{RUBY_VERSION}"
+    }
+  end
 
-  {
-    title: title,
-    avg: avg,
-    transaction_count: count,
-    executed_at: Time.new.iso8601,
-    'git.commit' => git_sha,
-    'git.date' => String(git_date).strip != '' && Time.parse(git_date).iso8601,
-    'git.subject' => git_msg,
-    hostname: `hostname`.chomp,
-    engine: RUBY_ENGINE,
-    arch: platform.cpu,
-    os: platform.os,
-    ruby_version: "#{RUBY_ENGINE == 'jruby' ? 'j' : ''}#{RUBY_VERSION}"
-  }
-end.compact
-
-STDERR.puts '=== Reporting to ES'
-STDERR.puts payloads.inspect
-
-payloads.each do |payload|
-  puts '{ "index" : { "_index" : "benchmark-ruby", "_type" : "_doc" } }'
-  puts payload.to_json
-end
+puts '{ "index" : { "_index" : "benchmark-ruby", "_type" : "_doc" } }'
+results.each { |result| puts result.to_json }
