c314148ea9ab0943278b49f50864afbb00e801bb
Mikkel Malmberg <mikkel@elastic.co>
2019-06-13 10:16:25 +0200

    Only add time if profiler enabled, rename keys

--- lib/elastic_apm/metrics/vm.rb
+++ lib/elastic_apm/metrics/vm.rb
@@ -11,18 +11,23 @@ module ElasticAPM
       # rubocop:disable Metrics/MethodLength
       def collect
         stat = GC.stat
-        @total_time += GC::Profiler.total_time
-        GC::Profiler.clear
         thread_count = Thread.list.count
 
-        {
+        sample = {
           'ruby.gc.count': stat[:count],
-          'ruby.gc.time': @total_time,
-          'ruby.heap.live': stat[:heap_live_slots],
-          'ruby.heap.free': stat[:heap_free_slots],
-          'ruby.allocations.total': stat[:total_allocated_objects],
+          'ruby.heap.slots.live': stat[:heap_live_slots],
+          'ruby.heap.slots.free': stat[:heap_free_slots],
+          'ruby.heap.allocations.total': stat[:total_allocated_objects],
           'ruby.threads': thread_count
         }
+
+        return sample unless GC::Profiler.enabled?
+
+        @total_time += GC::Profiler.total_time
+        GC::Profiler.clear
+        sample[:'ruby.gc.time'] = @total_time
+
+        sample
       end
       # rubocop:enable Metrics/MethodLength
     end
--- spec/elastic_apm/metrics/vm_spec.rb
+++ spec/elastic_apm/metrics/vm_spec.rb
@@ -5,20 +5,29 @@ module ElasticAPM
     RSpec.describe VM do
       let(:config) { Config.new }
 
-      before(:all) { GC::Profiler.enable }
-
       subject { described_class.new config }
 
       describe 'collect' do
         it 'collects a metric set and prefixes keys' do
           expect(subject.collect).to match(
             'ruby.gc.count': Integer,
-            'ruby.gc.time': Float,
-            'ruby.heap.live': Integer,
-            'ruby.heap.free': Integer,
-            'ruby.allocations.total': Integer,
+            'ruby.heap.slots.live': Integer,
+            'ruby.heap.slots.free': Integer,
+            'ruby.heap.allocations.total': Integer,
             'ruby.threads': Integer
           )
+        end
+
+        context 'with profiler enabled' do
+          around do |example|
+            GC::Profiler.enable
+            example.run
+            GC::Profiler.disable
+          end
+
+          it 'adds time spent' do
+            expect(subject.collect).to have_key(:'ruby.gc.time')
+          end
         end
       end
     end
