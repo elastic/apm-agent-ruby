c1ae6af029bfde82d720217bbccebd6fe1c7ba9e
Ivan Fernandez Calvo <kuisathaverat@users.noreply.github.com>
2019-06-25 19:07:00 +0200

    ci: fix syntax errors on downstream job (#455)

--- .ci/downstreamTests.groovy
+++ .ci/downstreamTests.groovy
@@ -30,9 +30,6 @@ pipeline {
     rateLimitBuilds(throttle: [count: 60, durationName: 'hour', userBoost: true])
     quietPeriod(10)
   }
-  triggers {
-    issueCommentTrigger('.*(?:jenkins\\W+)?run\\W+(?:the\\W+)?tests(?:\\W+please)?.*')
-  }
   parameters {
     string(name: 'RUBY_VERSION', defaultValue: "ruby-2.6", description: "Ruby version to test")
     string(name: 'BRANCH_SPECIFIER', defaultValue: "master", description: "Git branch/tag to use")
@@ -51,7 +48,7 @@ pipeline {
           branch: "${params.BRANCH_SPECIFIER}",
           repo: "${REPO}",
           credentialsId: "${JOB_GIT_CREDENTIALS}",
-          mergeTarget: "${params.CHANGE_TARGET}"
+          mergeTarget: "${params.CHANGE_TARGET}",
           reference: '/var/lib/jenkins/apm-agent-ruby.git')
         stash allowEmpty: true, name: 'source', useDefaultExcludes: false
       }
@@ -75,84 +72,85 @@ pipeline {
               tag: "Ruby",
               name: "Ruby",
               steps: this
-              )
-              def mapPatallelTasks = rubyTasksGen.generateParallelTests()
-              parallel(mapPatallelTasks)
-            }
+            )
+            def mapPatallelTasks = rubyTasksGen.generateParallelTests()
+            parallel(mapPatallelTasks)
           }
         }
-      }
-    post {
-      cleanup {
-        script{
-          if(rubyTasksGen?.results){
-            writeJSON(file: 'results.json', json: toJSON(rubyTasksGen.results), pretty: 2)
-            def mapResults = ["Ruby": rubyTasksGen.results]
-            def processor = new ResultsProcessor()
-            processor.processResults(mapResults)
-            archiveArtifacts allowEmptyArchive: true, artifacts: 'results.json,results.html', defaultExcludes: false
-          }
-        }
-        notifyBuildResult()
       }
     }
+  }
+  post {
+    cleanup {
+      script{
+        if(rubyTasksGen?.results){
+          writeJSON(file: 'results.json', json: toJSON(rubyTasksGen.results), pretty: 2)
+          def mapResults = ["Ruby": rubyTasksGen.results]
+          def processor = new ResultsProcessor()
+          processor.processResults(mapResults)
+          archiveArtifacts allowEmptyArchive: true, artifacts: 'results.json,results.html', defaultExcludes: false
+        }
+      }
+      notifyBuildResult()
+    }
+  }
+}
+
+/**
+Parallel task generator for the integration tests.
+*/
+class RubyParallelTaskGenerator extends DefaultParallelTaskGenerator {
+
+  public RubyParallelTaskGenerator(Map params){
+    super(params)
   }
 
   /**
-  Parallel task generator for the integration tests.
+  build a clousure that launch and agent and execute the corresponding test script,
+  then store the results.
   */
-  class RubyParallelTaskGenerator extends DefaultParallelTaskGenerator {
-
-    public RubyParallelTaskGenerator(Map params){
-      super(params)
-    }
-
-    /**
-    build a clousure that launch and agent and execute the corresponding test script,
-    then store the results.
-    */
-    public Closure generateStep(x, y){
-      return {
-        steps.sleep steps.randomNumber(min:10, max: 30)
-        steps.node('linux && immutable'){
-          // Label is transformed to avoid using the internal docker registry in the x coordinate
-          // TODO: def label = "${tag}:${x?.drop(x?.lastIndexOf('/')+1)}#${y}"
-          def label = "${tag}:${x}#${y}"
-          try {
-            steps.runScript(label: label, ruby: x, framework: y)
-            saveResult(x, y, 1)
-          } catch(e){
-            saveResult(x, y, 0)
-            steps.error("${label} tests failed : ${e.toString()}\n")
-          } finally {
-            steps.junit(allowEmptyResults: false,
-              keepLongStdio: true,
-              testResults: "**/spec/ruby-agent-junit.xml")
-            steps.codecov(repo: "${steps.env.REPO}", basedir: "${steps.env.BASE_DIR}",
-              secret: "${steps.env.CODECOV_SECRET}")
-          }
+  public Closure generateStep(x, y){
+    return {
+      steps.sleep steps.randomNumber(min:10, max: 30)
+      steps.node('linux && immutable'){
+        // Label is transformed to avoid using the internal docker registry in the x coordinate
+        // TODO: def label = "${tag}:${x?.drop(x?.lastIndexOf('/')+1)}#${y}"
+        def label = "${tag}:${x}#${y}"
+        try {
+          steps.runScript(label: label, ruby: x, framework: y)
+          saveResult(x, y, 1)
+        } catch(e){
+          saveResult(x, y, 0)
+          steps.error("${label} tests failed : ${e.toString()}\n")
+        } finally {
+          steps.junit(allowEmptyResults: false,
+            keepLongStdio: true,
+            testResults: "**/spec/ruby-agent-junit.xml")
+          steps.codecov(repo: "${steps.env.REPO}", basedir: "${steps.env.BASE_DIR}",
+            secret: "${steps.env.CODECOV_SECRET}")
         }
       }
     }
   }
+}
 
-  /**
-    Run tests for a Ruby version and framework version.
-  */
-  def runScript(Map params = [:]){
-    def label = params.label
-    def ruby = params.ruby
-    def framework = params.framework
-    log(level: 'INFO', text: "${label}")
-    env.HOME = "${env.WORKSPACE}"
-    env.PATH = "${env.PATH}:${env.WORKSPACE}/bin"
-    deleteDir()
-    unstash 'source'
-    dir("${BASE_DIR}"){
-      retry(2){
-        sleep randomNumber(min:10, max: 30)
-        dockerLogin(secret: "${DOCKER_SECRET}", registry: "${DOCKER_REGISTRY}")
-        sh("./spec/scripts/spec.sh ${ruby} ${framework}")
-      }
+/**
+  Run tests for a Ruby version and framework version.
+*/
+def runScript(Map params = [:]){
+  def label = params.label
+  def ruby = params.ruby
+  def framework = params.framework
+  log(level: 'INFO', text: "${label}")
+  env.HOME = "${env.WORKSPACE}"
+  env.PATH = "${env.PATH}:${env.WORKSPACE}/bin"
+  deleteDir()
+  unstash 'source'
+  dir("${BASE_DIR}"){
+    retry(2){
+      sleep randomNumber(min:10, max: 30)
+      dockerLogin(secret: "${DOCKER_SECRET}", registry: "${DOCKER_REGISTRY}")
+      sh("./spec/scripts/spec.sh ${ruby} ${framework}")
     }
   }
+}
