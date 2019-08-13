6142752cbda7be9300ab6db375864f1acf841dad
Emily Stolfo <emstolfo@gmail.com>
2019-08-13 11:23:00 +0200

    Support chained exceptions

--- spec/support/exception_helpers.rb
+++ spec/support/exception_helpers.rb
@@ -6,4 +6,20 @@ module ExceptionHelpers
   rescue => e # rubocop:disable Style/RescueStandardError
     e
   end
+
+  # rubocop:disable Metrics/MethodLength
+  def actual_chained_exception
+    1 / 0
+  rescue ZeroDivisionError
+    begin
+      File.open('gotcha')
+    rescue Errno::ENOENT
+      begin
+        [].merge
+      rescue NoMethodError => e
+        e
+      end
+    end
+  end
+  # rubocop:enable Metrics/MethodLength
 end
