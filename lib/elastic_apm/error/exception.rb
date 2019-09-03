b43e37862ec6b822b51232dc4c199ebb98e174c1
Mikkel Malmberg <mikkel@brnbw.com>
2019-09-03 13:07:16 +0200

    Remove type from exception message (#323)

--- lib/elastic_apm/error/exception.rb
+++ lib/elastic_apm/error/exception.rb
@@ -6,31 +6,36 @@ module ElasticAPM
     class Exception
       MOD_SPLIT = '::'
 
-      def initialize(exception, **attrs)
-        @message =
-          "#{exception.class}: #{exception.message}"
-        @type = exception.class.to_s
-        @module = format_module exception
+      def initialize(attrs = nil)
+        return unless attrs
 
         attrs.each do |key, val|
           send(:"#{key}=", val)
         end
       end
 
-      attr_accessor(
-        :attributes,
-        :code,
-        :handled,
-        :message,
-        :module,
-        :stacktrace,
-        :type
-      )
+      def self.from_exception(exception, **attrs)
+        new({
+          message: exception.message.to_s,
+          type: exception.class.to_s,
+          module: format_module(exception)
+        }.merge(attrs))
+      end
 
-      private
+      attr_accessor :attributes
+      attr_accessor :code
+      attr_accessor :handled
+      attr_accessor :message
+      attr_accessor :module
+      attr_accessor :stacktrace
+      attr_accessor :type
 
-      def format_module(exception)
-        exception.class.to_s.split(MOD_SPLIT)[0...-1].join(MOD_SPLIT)
+      class << self
+        private
+
+        def format_module(exception)
+          exception.class.to_s.split(MOD_SPLIT)[0...-1].join(MOD_SPLIT)
+        end
       end
     end
   end
