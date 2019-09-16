6b6aa94fb1af2baae4319884ed4556377d798578
Emily Stolfo <emstolfo@gmail.com>
2019-09-16 14:04:47 +0200

    No reason to embed spec in ElasticAPM module

--- spec/elastic_apm/rails_spec.rb
+++ spec/elastic_apm/rails_spec.rb
@@ -2,43 +2,41 @@
 
 if defined?(Rails)
   require 'elastic_apm/rails'
-  module ElasticAPM
-    RSpec.describe Rails do
-      describe '.start' do
-        before :all do
-          ElasticAPM::Rails.start({})
-        end
-
-        it 'starts the agent' do
-          expect(ElasticAPM::Agent).to be_running
-        end
-
-        it 'registers the ActionDispatchSpy' do
-          expect(ElasticAPM::Agent).to be_running
-        end
-
-        after :all do
-          ElasticAPM.stop
-        end
+  RSpec.describe Rails do
+    describe '.start' do
+      before :all do
+        ElasticAPM::Rails.start({})
       end
 
-      describe 'Rails console' do
-        before :all do
-          module ::Rails
-            class Console; end
-          end
+      it 'starts the agent' do
+        expect(ElasticAPM::Agent).to be_running
+      end
 
-          ElasticAPM::Rails.start({})
+      it 'registers the ActionDispatchSpy' do
+        expect(ElasticAPM::Agent).to be_running
+      end
+
+      after :all do
+        ElasticAPM.stop
+      end
+    end
+
+    describe 'Rails console' do
+      before :all do
+        module Rails
+          class Console; end
         end
 
-        after :all do
-          ElasticAPM.stop
-          ::Rails.send(:remove_const, :Console)
-        end
+        ElasticAPM::Rails.start({})
+      end
 
-        it "doesn't start when console" do
-          expect(ElasticAPM.agent).to be nil
-        end
+      after :all do
+        ElasticAPM.stop
+        Rails.send(:remove_const, :Console)
+      end
+
+      it "doesn't start when console" do
+        expect(ElasticAPM.agent).to be nil
       end
     end
   end
