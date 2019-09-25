25db3afbcd267ebeccb4ab51b2b1612a575ea584
Mikkel Malmberg <mikkel@elastic.co>
2019-09-25 14:38:00 +0200

    Smallest possible fix for jruby+intercept

--- spec/support/intercept.rb
+++ spec/support/intercept.rb
@@ -6,15 +6,17 @@ RSpec.configure do |config|
       @transactions = []
       @spans = []
       @errors = []
+      @metricsets = []
     end
 
-    attr_reader :transactions, :spans, :errors
+    attr_reader :transactions, :spans, :errors, :metricsets
   end
 
   config.before :each, intercept: true do
     @intercepted = Intercept.new
 
-    allow_any_instance_of(ElasticAPM::Agent).to receive(:enqueue) do |_, obj|
+    allow_any_instance_of(ElasticAPM::Transport::Base)
+      .to receive(:submit) do |_, obj|
       case obj
       when ElasticAPM::Transaction
         @intercepted.transactions << obj
@@ -22,9 +24,15 @@ RSpec.configure do |config|
         @intercepted.spans << obj
       when ElasticAPM::Error
         @intercepted.errors << obj
+      when ElasticAPM::Metricset
+        @intercepted.metricsets << obj
       end
 
       true
     end
+  end
+
+  config.after :each, intercept: true do
+    @intercepted = nil
   end
 end
