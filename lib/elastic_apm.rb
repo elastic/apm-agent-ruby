8e8cde1c10b849783b30ef11cfca1ba8c166694b
Emily Stolfo <emstolfo@gmail.com>
2019-09-26 15:10:45 +0200

    Only allow certain label types to be set, otherwise call to_s

--- lib/elastic_apm.rb
+++ lib/elastic_apm.rb
@@ -66,7 +66,8 @@ module ElasticAPM # rubocop:disable Metrics/ModuleLength
     # Get a formatted string containing transaction, span, and trace ids.
     # If a block is provided, the ids are yielded.
     #
-    # @yield [String|nil, String|nil, String|nil] The transaction, span, and trace ids.
+    # @yield [String|nil, String|nil, String|nil] The transaction, span,
+    # and trace ids.
     # @return [String] Unless block given
     def log_ids
       trace_id = (current_transaction || current_span)&.trace_id
@@ -376,7 +377,16 @@ module ElasticAPM # rubocop:disable Metrics/ModuleLength
     # @param value [Object] A value
     # @return [Object] The given value
     def set_label(key, value)
-      agent&.set_label(key, value)
+      case value
+      when TrueClass,
+           FalseClass,
+           Numeric,
+           NilClass,
+           String
+        agent&.set_label(key, value)
+      else
+        agent&.set_label(key, value.to_s)
+      end
     end
 
     # Provide further context for the current transaction
