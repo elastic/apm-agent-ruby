3d729c6248c43f36b517892503f8840830b3b839
Emily Stolfo <emstolfo@gmail.com>
2019-10-14 11:44:52 +0200

    Test passing config values to Grape.start

--- spec/elastic_apm/grape_spec.rb
+++ spec/elastic_apm/grape_spec.rb
@@ -4,20 +4,33 @@ if defined?(Grape)
   require 'elastic_apm/grape'
   RSpec.describe Grape do
     describe '.start' do
-      before :all do
+
+      before do
         class GrapeTestApp < ::Grape::API
           use ElasticAPM::Middleware
         end
-        ElasticAPM::Grape.start(GrapeTestApp, {})
+        ElasticAPM::Grape.start(GrapeTestApp, config)
       end
 
-      it 'starts the agent' do
-        expect(ElasticAPM::Agent).to be_running
-      end
-
-      after :all do
+      after do
         ElasticAPM.stop
         Object.send(:remove_const, :GrapeTestApp)
+      end
+
+      context 'with no overridden config settings' do
+        let(:config) { {} }
+        it 'starts the agent' do
+          expect(ElasticAPM::Agent).to be_running
+        end
+      end
+
+      context 'a config with settings' do
+        let(:config) { { service_name: 'Other Name' } }
+
+        it 'sets the options' do
+          expect(ElasticAPM.agent.config.options[:service_name].value).
+            to eq('Other Name')
+        end
       end
     end
   end
