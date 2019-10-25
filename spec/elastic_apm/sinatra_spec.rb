e3228f45e9ade4295004750dd7695cd60edbfaa0
Mikkel Malmberg <mikkel@elastic.co>
2019-10-25 13:22:16 +0200

    Fix ElasticAPM::Sinatra spec

--- spec/elastic_apm/sinatra_spec.rb
+++ spec/elastic_apm/sinatra_spec.rb
@@ -1,34 +1,41 @@
 # frozen_string_literal: true
 
 if defined?(Sinatra)
-  require 'elastic_apm/sinatra'
   RSpec.describe Sinatra do
-    describe '.start' do
+    describe '.start', :intercept do
       before do
         class SinatraTestApp < ::Sinatra::Base
           use ElasticAPM::Middleware
         end
-        ElasticAPM::Sinatra.start(SinatraTestApp, config)
       end
 
       after do
-        ElasticAPM.stop
         Object.send(:remove_const, :SinatraTestApp)
       end
 
       context 'with no overridden config settings' do
-        let(:config) { {} }
         it 'starts the agent' do
-          expect(ElasticAPM::Agent).to be_running
+          begin
+            ElasticAPM::Sinatra.start(SinatraTestApp)
+            expect(ElasticAPM::Agent).to be_running
+            expect(ElasticAPM.agent.config.options[:service_name].value)
+              .to eq 'SinatraTestApp'
+          ensure
+            ElasticAPM.stop
+          end
         end
       end
 
       context 'a config with settings' do
-        let(:config) { { service_name: 'Other Name' } }
-
         it 'sets the options' do
-          expect(ElasticAPM.agent.config.options[:service_name].value)
-            .to eq('Other Name')
+          begin
+            ElasticAPM::Sinatra.start(SinatraTestApp, service_name: 'my-app')
+            expect(ElasticAPM::Agent).to be_running
+            expect(ElasticAPM.agent.config.options[:service_name].value)
+              .to eq 'my-app'
+          ensure
+            ElasticAPM.stop
+          end
         end
       end
     end
