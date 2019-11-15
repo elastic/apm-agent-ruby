1de8f0a8499554cb59d65bfd9378f04765e4fe26
Mikkel Malmberg <mikkel@elastic.co>
2019-11-15 11:31:37 +0100

    Always stub CC when with_agent

--- spec/support/with_agent.rb
+++ spec/support/with_agent.rb
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 module WithAgent
+  # rubocop:disable Metrics/MethodLength
   def with_agent(klass: ElasticAPM, args: [], **config)
     unless @mock_intake || @intercepted
       raise 'Using with_agent but neither MockIntake nor Intercepted'
     end
 
+    @central_config_stub ||=
+      WebMock.stub_request(
+        :get, %r{^http://localhost:8200/config/v1/agents/?$}
+      ).to_return(body: '{}')
+
     klass.start(*args, **config)
     yield
   ensure
     ElasticAPM.stop
+
+    @central_config_stub = nil
   end
+  # rubocop:enable Metrics/MethodLength
 end
