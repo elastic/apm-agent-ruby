33064ee37602e649e10eea45a9286e709726410c
Niklas Riekenbrauck <nr@mtomady.com>
2019-12-10 16:08:03 +0100

    Add shoryuken instrumentation

---
+++ spec/elastic_apm/spies/shoryuken_spec.rb
@@ -0,0 +1,106 @@
+# frozen_string_literal: true
+
+require 'shoryuken'
+
+require 'elastic_apm/spies/shoryuken'
+
+begin
+  require 'active_job'
+rescue LoadError
+end
+
+
+module ElasticAPM
+  RSpec.describe 'Spy: Shoryuken', :mock_intake do
+    class TestingWorker
+      include Shoryuken::Worker
+
+      shoryuken_options queue: 'hello'
+
+      def perform
+        'ok'
+      end
+    end
+
+    class HardWorker < TestingWorker; end
+    class ExplodingWorker < TestingWorker
+      def perform
+        super
+        1 / 0
+      end
+    end
+
+    before :all do
+      Shoryuken.worker_executor = Shoryuken::Worker::InlineExecutor
+    end
+
+    subject(:call) do
+      worker.perform(sqs_msg, body)
+    end
+
+    let(:worker) { TestingWorker.new }
+
+    it 'instruments jobs' do
+      with_agent do
+        HardWorker.perform_async
+      end
+
+      wait_for transactions: 1
+
+      transaction, = @mock_intake.transactions
+      expect(transaction).to_not be_nil
+      expect(transaction['name']).to eq 'ElasticAPM::HardWorker'
+      expect(transaction['type']).to eq 'Sidekiq'
+    end
+
+    it 'reports errors' do
+      with_agent do
+          expect do
+            ExplodingWorker.perform_async
+          end.to raise_error(ZeroDivisionError)
+      end
+
+      wait_for transactions: 1, errors: 1
+
+      transaction, = @mock_intake.transactions
+      error, = @mock_intake.errors
+
+      expect(transaction).to_not be_nil
+      expect(transaction['name']).to eq 'ElasticAPM::ExplodingWorker'
+      expect(transaction['type']).to eq 'Sidekiq'
+
+      expect(error.dig('exception', 'type')).to eq 'ZeroDivisionError'
+    end
+
+    context 'ActiveJob', if: defined?(ActiveJob) do
+      before :all do
+        # rubocop:disable Style/ClassAndModuleChildren
+        class ::ActiveJobbyJob < ActiveJob::Base
+          # rubocop:enable Style/ClassAndModuleChildren
+          self.queue_adapter = :shoryuken
+          self.logger = nil # stay quiet
+
+          def perform
+            'ok'
+          end
+        end
+      end
+
+      after :all do
+        Object.send(:remove_const, :ActiveJobbyJob)
+      end
+
+      it 'knows the name of ActiveJob jobs', if: defined?(ActiveJob) do
+        with_agent do
+          ActiveJobbyJob.perform_later
+        end
+
+        wait_for transactions: 1
+
+        transaction, = @mock_intake.transactions
+        expect(transaction).to_not be_nil
+        expect(transaction['name']).to eq 'ActiveJobbyJob'
+      end
+    end
+  end
+end
