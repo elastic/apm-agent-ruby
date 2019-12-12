f08823c422fc3c22813393db7d6882d2777a20ce
Emily Stolfo <emstolfo@gmail.com>
2019-12-12 16:23:32 +0100

    Fix rubocop offenses

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,34 +1,36 @@
-When("an api key is set to {string} in the config") do |string|
+# frozen_string_literal: true
+
+When('an api key is set to {string} in the config') do |string|
   @config ||= ElasticAPM::Config.new
   @config.api_key = string
   @api_key = string
 end
 
-Then("the Authorization header includes the api key as a Base64 encoded string") do
+Then('the Authorization header has the api key as a Base64 encoded string') do
   headers = ElasticAPM::Transport::Headers.new(@config).to_h
   headers[:Authorization].include?(Base64.strict_encode64(@api_key))
 end
 
-When("an api key is set in the config") do
+When('an api key is set in the config') do
   @config ||= ElasticAPM::Config.new
   @config.api_key = '123:456'
 end
 
-Then("the api key is sent in the Authorization header") do
+Then('the api key is sent in the Authorization header') do
   headers = ElasticAPM::Transport::Headers.new(@config).to_h
   headers[:Authorization].include?(@config.api_key)
 end
 
-When("a secret_token is set in the config") do
+When('a secret_token is set in the config') do
   @config ||= ElasticAPM::Config.new
   @config.secret_token = 'abcde'
 end
 
-When("an api key is not set in the config") do
+When('an api key is not set in the config') do
   @config ||= ElasticAPM::Config.new
 end
 
-Then("the secret token is sent in the Authorization header") do
+Then('the secret token is sent in the Authorization header') do
   headers = ElasticAPM::Transport::Headers.new(@config).to_h
   headers[:Authorization].include?(@config.secret_token)
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'cucumber/rspec/doubles'
 require 'pry-nav'
 require 'elastic-apm'
