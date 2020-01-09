263ef8f4947e9711b962330153223a2725b6de9b
Emily Stolfo <emstolfo@gmail.com>
2020-01-09 10:36:01 +0100

    Use Helpers to refer to config and add After block

--- features/step_definitions/stepdefs.rb
+++ features/step_definitions/stepdefs.rb
@@ -1,38 +1,47 @@
 # frozen_string_literal: true
 
+module Helpers
+  def config
+    @agent.config
+  end
+end
+
+World(Helpers)
+
+After do
+  @agent.stop if @agent
+end
+
 Given('an agent') do
+  @agent = ElasticAPM.start
 end
 
 When('an api key is set to {string} in the config') do |api_key|
-  @config ||= ElasticAPM::Config.new
-  @config.api_key = api_key
+  config.api_key = api_key
 end
 
 Then('the Authorization header is {string}') do |auth_header|
-  headers = ElasticAPM::Transport::Headers.new(@config).to_h
+  headers = ElasticAPM::Transport::Headers.new(config).to_h
   headers[:Authorization] == auth_header
 end
 
 When('an api key is set in the config') do
-  @config ||= ElasticAPM::Config.new
-  @config.api_key = '123:456'
+  config.api_key = '123:456'
 end
 
 Then('the api key is sent in the Authorization header') do
-  headers = ElasticAPM::Transport::Headers.new(@config).to_h
-  headers[:Authorization].include?(@config.api_key)
+  headers = ElasticAPM::Transport::Headers.new(config).to_h
+  headers[:Authorization].include?(config.api_key)
 end
 
 When('a secret_token is set in the config') do
-  @config ||= ElasticAPM::Config.new
-  @config.secret_token = 'abcde'
+  config.secret_token = 'abcde'
 end
 
 When('an api key is not set in the config') do
-  @config ||= ElasticAPM::Config.new
 end
 
 Then('the secret token is sent in the Authorization header') do
-  headers = ElasticAPM::Transport::Headers.new(@config).to_h
-  headers[:Authorization].include?(@config.secret_token)
+  headers = ElasticAPM::Transport::Headers.new(config).to_h
+  headers[:Authorization].include?(config.secret_token)
 end
