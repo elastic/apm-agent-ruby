6431652e5b7ba98e613eb174bb930f2984c9c58d
Victor Martinez <victormartinezrubio@gmail.com>
2020-01-20 14:32:26 +0000

    [benchmark] some fixes in the CI (#692)

--- spec/scripts/benchmarks.sh
+++ spec/scripts/benchmarks.sh
@@ -5,15 +5,17 @@
 #
 # Usage: ./spec/scripts/benchmarks.sh jruby:9.1
 #
-set -exuo pipefail
+set -exo pipefail
 
-if [ $# -lt 1 ]; then
-  echo "Arguments missing"
-  exit 2
-fi
-
-RUBY_IMAGE=${1}
+RUBY_IMAGE=${1:?"missing RUBY IMAGE"}
 VERSION=$(echo "${RUBY_IMAGE}" | cut -d":" -f2)
+REFERENCE_REPO=${2}
+
+if [ -z "${REFERENCE_REPO}" ] ; then
+  REFERENCE_REPO_FLAG=""
+else
+  REFERENCE_REPO_FLAG="-v ${REFERENCE_REPO}:${REFERENCE_REPO}"
+fi
 
 ## Transform the versions like:
 ##  - docker.elastic.co/observability-ci/jruby:9.2-12-jdk to jruby-9.2-12-jdk
@@ -30,17 +32,20 @@ cd spec
 docker build --pull --force-rm --build-arg "RUBY_IMAGE=${RUBY_IMAGE}" -t "apm-agent-ruby:${VERSION}" .
 RUBY_VERSION=${VERSION} docker-compose run \
   --user $UID \
-  -e HOME=/app \
+  -e HOME=/tmp \
   -e FRAMEWORK=rails \
   -w /app \
   -e LOCAL_USER_ID=$UID \
   -v "$local_vendor_path:$container_vendor_path" \
   -v "$(dirname "$(pwd)"):/app" \
+  ${REFERENCE_REPO_FLAG} \
   --rm ruby_rspec \
   /bin/bash -c "set -x
+    cp -rf ${container_vendor_path} /tmp/.vendor
     gem update --system
     gem install bundler
-    bundle install --path $container_vendor_path
+    bundle config --global set path '/tmp/.vendor'
+    bundle install
     bench/benchmark.rb 2> benchmark-${TRANSFORMED_VERSION}.error > benchmark-${TRANSFORMED_VERSION}.raw
     bench/report.rb < benchmark-${TRANSFORMED_VERSION}.raw > benchmark-${TRANSFORMED_VERSION}.bulk"
 
