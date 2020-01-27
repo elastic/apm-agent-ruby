a624c4d7385e8600ae75865d7a25e5eaf10b4b5c
Victor Martinez <VictorMartinezRubio@gmail.com>
2020-01-27 12:01:26 +0000

    Pull and retag docker images

--- .ci/packer_cache.sh
+++ .ci/packer_cache.sh
@@ -5,5 +5,8 @@ source /usr/local/bin/bash_standard_lib.sh
 grep "-" .ci/.jenkins_ruby.yml | cut -d'-' -f2- | \
 while read -r version;
 do
-    (retry 2 ./spec/scripts/docker_build.sh "${version}") || echo "Error building ${version} Docker image, we continue"
+    imageName="apm-agent-ruby:${version}"
+    registryImageName="docker.elastic.co/observability-ci/${imageName}"
+    (retry 2 docker pull "${registryImageName}")
+    docker tag "${registryImageName}" "${imageName}"
 done
--- spec/scripts/docker_build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-RUBY_IMAGE=${1}
-VERSION=$(echo "${RUBY_IMAGE}" | cut -d":" -f2)
-
-cd spec
-docker build --build-arg "RUBY_IMAGE=${RUBY_IMAGE}" -t "apm-agent-ruby:${VERSION}" .
