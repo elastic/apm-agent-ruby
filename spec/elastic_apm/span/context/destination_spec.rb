d57992c297afa9d011990f5aba3a4adc7bf212ff
Emily Stolfo <emstolfo@gmail.com>
2020-02-24 14:17:57 +0100

    Support address and port in context.destination

--- spec/elastic_apm/span/context/destination_spec.rb
+++ spec/elastic_apm/span/context/destination_spec.rb
@@ -12,17 +12,23 @@ module ElasticAPM
           its(:name) { is_expected.to eq 'http://example.com' }
           its(:resource) { is_expected.to eq 'example.com:80' }
           its(:type) { is_expected.to eq 'external' }
+          its(:address) { is_expected.to eq 'example.com' }
+          its(:port) { is_expected.to eq 80 }
 
           context 'https' do
             let(:uri) { URI('https://example.com/path?a=1') }
             its(:name) { is_expected.to eq 'https://example.com' }
             its(:resource) { is_expected.to eq 'example.com:443' }
+            its(:address) { is_expected.to eq 'example.com' }
+            its(:port) { is_expected.to eq 443 }
           end
 
           context 'non-default port' do
             let(:uri) { URI('http://example.com:8080/path?a=1') }
             its(:name) { is_expected.to eq 'http://example.com:8080' }
             its(:resource) { is_expected.to eq 'example.com:8080' }
+            its(:address) { is_expected.to eq 'example.com' }
+            its(:port) { is_expected.to eq 8080 }
           end
 
           context 'when given a string' do
@@ -31,6 +37,18 @@ module ElasticAPM
             its(:name) { is_expected.to eq 'http://example.com' }
             its(:resource) { is_expected.to eq 'example.com:80' }
             its(:type) { is_expected.to eq 'external' }
+            its(:address) { is_expected.to eq 'example.com' }
+            its(:port) { is_expected.to eq 80 }
+          end
+
+          context 'IPv6' do
+            let(:uri) { 'http://[::1]:8080/' }
+
+            its(:name) { is_expected.to eq 'http://[::1]:8080' }
+            its(:resource) { is_expected.to eq '[::1]:8080' }
+            its(:type) { is_expected.to eq 'external' }
+            its(:address) { is_expected.to eq '::1' }
+            its(:port) { is_expected.to eq 8080 }
           end
         end
       end
