21010a8ab99c8d314b731452c4693daada582f6e
Mikkel Malmberg <mikkel@elastic.co>
2020-08-03 11:39:44 +0200

    ES Spy: Return early when no agent/transaction (#830)

--- spec/elastic_apm/spies/elasticsearch_spec.rb
+++ spec/elastic_apm/spies/elasticsearch_spec.rb
@@ -20,10 +20,20 @@
 require 'spec_helper'
 
 require 'elasticsearch'
+require 'elastic_apm/spies/elasticsearch'
 
 module ElasticAPM
   RSpec.describe 'Spy: Elasticsearch' do
     after { WebMock.reset! }
+
+    it 'calls through with no transaction', :intercept do
+      req_stub = WebMock.stub_request(:get, %r{http://localhost:9200/.*})
+
+      client = Elasticsearch::Client.new log: false
+      client.search q: 'test'
+
+      expect(req_stub).to have_been_requested
+    end
 
     it 'spans requests', :intercept do
       WebMock.stub_request(:get, %r{http://localhost:9200/.*})
