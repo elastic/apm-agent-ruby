d0b91f5f01846ab4fc61006f8e17604ceecce188
Mikkel Malmberg <mikkel@elastic.co>
2020-08-05 14:37:23 +0200

    DelayedJob spy: Simplify job name lookup

--- lib/elastic_apm/spies/delayed_job.rb
+++ lib/elastic_apm/spies/delayed_job.rb
@@ -38,8 +38,8 @@ module ElasticAPM
       end
 
       def self.invoke_job(job, *args, &block)
-        job_name = name_from_payload(job.payload_object)
-        transaction = ElasticAPM.start_transaction(job_name, TYPE)
+        name = job_name(job)
+        transaction = ElasticAPM.start_transaction(name, TYPE)
         job.invoke_job_without_apm(*args, &block)
         transaction.done 'success'
       rescue ::Exception => e
@@ -50,29 +50,12 @@ module ElasticAPM
         ElasticAPM.end_transaction
       end
 
-      def self.name_from_payload(payload_object)
-        if payload_object.is_a?(::Delayed::PerformableMethod)
-          performable_method_name(payload_object)
+      def self.job_name(job)
+        if job.payload_object.is_a?(::Delayed::PerformableMethod)
+          job.name
         else
-          payload_object.class.name
+          job.payload_object.class.name
         end
-      end
-
-      def self.performable_method_name(payload_object)
-        class_name = object_name(payload_object)
-        separator = name_separator(payload_object)
-        method_name = payload_object.method_name
-        "#{class_name}#{separator}#{method_name}"
-      end
-
-      def self.object_name(payload_object)
-        object = payload_object.object
-        klass = object.is_a?(Class) ? object : object.class
-        klass.name
-      end
-
-      def self.name_separator(payload_object)
-        payload_object.object.is_a?(Class) ? CLASS_SEPARATOR : METHOD_SEPARATOR
       end
     end
 
