b9846fb5804a2b4912cd650f8b9d6c33cc6c6d12
Manuel de la Peña <mdelapenya@gmail.com>
2020-11-06 17:42:09 +0100

    feat: automate the execution of BDD tests
    It supports two modes:
    1. local mode, with the "run-bdd", which uses local dependencies to run
    the tests (it requires "bundle install")
    2. docker mode, which runs the tests inside a Ruby container

---
+++ spec/scripts/features.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+#
+# Test the given docker ruby image. Optionally to filter what test
+# to be triggered otherwise all of them.
+#
+# NOTE: It's required to be launched inside the root of the project.
+#
+# Usage:
+#   All feature files: ./spec/scripts/features.sh jruby:9.1 sinatra-2.0
+#   One feature file: ./spec/scripts/features.sh jruby:9.1 sinatra-2.0 "features/step_definitions/stepdefs.rb"
+#
+set -ex
+
+if [ $# -lt 1 ]; then
+  echo "Arguments missing"
+  exit 2
+fi
+
+IMAGE_NAME=${1}
+TEST=${2}
+VERSION=$(echo "${IMAGE_NAME}" | cut -d":" -f2)
+
+cd spec
+
+IMAGE_NAME=${IMAGE_NAME} RUBY_VERSION=${VERSION} USER_ID="$(id -u):$(id -g)" docker-compose up -d mongodb
+
+## Customise the docker container to enable the access to the internal of the jdk
+## for the jruby docker images.
+JDK_JAVA_OPTIONS=''
+JRUBY_OPTS=''
+if [[ $RUBY_IMAGE == *"jruby"* ]]; then
+  # https://github.com/jruby/jruby/issues/4834#issuecomment-343371742
+  JDK_JAVA_OPTIONS="--illegal-access=permit $(echo --add-opens=java.base/{java.lang,java.security,java.util,java.security.cert,java.util.zip,java.lang.reflect,java.util.regex,java.net,java.io,java.lang}=ALL-UNNAMED)"
+  JRUBY_OPTS="--debug"
+fi
+
+CLEAN_IMAGE_NAME=$(echo $IMAGE_NAME | sed s/:/-/ )
+
+docker build --build-arg "RUBY_IMAGE=${IMAGE_NAME}" -t "apm-agent-ruby:${VERSION}" .
+
+IMAGE_NAME=${IMAGE_NAME} RUBY_VERSION=${VERSION} USER_ID="$(id -u):$(id -g)" \
+  docker-compose -f ../docker-compose.yml run \
+  -e INCLUDE_SCHEMA_SPECS=1 \
+  -e JDK_JAVA_OPTIONS="${JDK_JAVA_OPTIONS}" \
+  -e JRUBY_OPTS="${JRUBY_OPTS}" \
+  -e HOME="/tmp" \
+  -v "$(dirname "$(pwd)"):/app" \
+  -w /app \
+  --rm ruby_rspec \
+  /bin/bash -c "\
+    gem install rake && \
+    bundle update && \
+    timeout -s9 15m bin/run-bdd ${TEST}"
