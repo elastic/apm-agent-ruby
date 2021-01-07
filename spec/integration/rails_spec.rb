943201889c8a1af80aca8c8b126d3a328db23ff7
Emily Stolfo <emily.stolfo@elastic.co>
2021-01-07 12:06:43 +0100

    Only test ruby 3.0 with rails 6.0

--- spec/integration/rails_spec.rb
+++ spec/integration/rails_spec.rb
@@ -319,7 +319,7 @@ if enabled
         end
       end
 
-      context 'when json' do
+      context 'when json', unless: RUBY_VERSION >= '3.0' do
         it 'validates the schema' do
           get '/error'
 
