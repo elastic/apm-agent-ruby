01ee881b272efeb65d8866d4e0e40e70d73cc671
Mikkel Malmberg <mikkel@elastic.co>
2021-07-09 13:44:26 +0200

    Only run test if jruby

--- spec/elastic_apm/metrics/jvm_set_spec.rb
+++ spec/elastic_apm/metrics/jvm_set_spec.rb
@@ -19,53 +19,55 @@
 
 require 'spec_helper'
 
-module ElasticAPM
-  module Metrics
-    RSpec.describe JVMSet, if: defined?(JRUBY_VERSION) do
-      let(:config) { Config.new }
+if defined?(JRUBY_VERSION)
+  module ElasticAPM
+    module Metrics
+      RSpec.describe JVMSet do
+        let(:config) { Config.new }
 
-      subject { described_class.new config }
+        subject { described_class.new config }
 
-      describe 'collect' do
-        context 'when disabled' do
-          it 'returns' do
-            subject.disable!
-            expect(subject.collect).to be nil
+        describe 'collect' do
+          context 'when disabled' do
+            it 'returns' do
+              subject.disable!
+              expect(subject.collect).to be nil
+            end
           end
-        end
 
-        it 'disables after three errors' do
-          allow(java.lang.management.ManagementFactory).to receive(:getPlatformMXBean).and_raise(Exception)
+          it 'disables after three errors' do
+            allow(java.lang.management.ManagementFactory).to receive(:getPlatformMXBean).and_raise(Exception)
 
-          2.times do
+            2.times do
+              subject.collect
+              expect(subject).to_not be_disabled
+            end
+
             subject.collect
-            expect(subject).to_not be_disabled
+            expect(subject).to be_disabled
           end
 
-          subject.collect
-          expect(subject).to be_disabled
-        end
+          it 'collects a metric set and prefixes keys' do
+            subject.collect
+            sleep 0.2
+            sets = subject.collect
 
-        it 'collects a metric set and prefixes keys' do
-          subject.collect
-          sleep 0.2
-          sets = subject.collect
-
-          expect(sets.first.samples).to match(
-            :"jvm.memory.heap.used" => Integer,
-            :"jvm.memory.heap.committed" => Integer,
-            :"jvm.memory.heap.max" => Integer,
-            :"jvm.memory.non_heap.used" => Integer,
-            :"jvm.memory.non_heap.committed" => Integer,
-            :"jvm.memory.non_heap.max" => Integer,
-          )
-          sets[1..-1].each do |set|
-            expect(set.tags).to match(name: String)
-            expect(set.samples).to match(
-              :"jvm.memory.heap.pool.used" => Integer,
-              :"jvm.memory.heap.pool.committed" => Integer,
-              :"jvm.memory.heap.pool.max" => Integer,
+            expect(sets.first.samples).to match(
+              :"jvm.memory.heap.used" => Integer,
+              :"jvm.memory.heap.committed" => Integer,
+              :"jvm.memory.heap.max" => Integer,
+              :"jvm.memory.non_heap.used" => Integer,
+              :"jvm.memory.non_heap.committed" => Integer,
+              :"jvm.memory.non_heap.max" => Integer,
             )
+            sets[1..-1].each do |set|
+              expect(set.tags).to match(name: String)
+              expect(set.samples).to match(
+                :"jvm.memory.heap.pool.used" => Integer,
+                :"jvm.memory.heap.pool.committed" => Integer,
+                :"jvm.memory.heap.pool.max" => Integer,
+              )
+            end
           end
         end
       end
