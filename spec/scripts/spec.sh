7002e71eec8ad5fcf0cd8f8fa91eb660aa1156d8
Justin George <justin.george@elastic.co>
2022-05-11 18:04:35 -0700

    Omit extra bundler install in scripts/spec

--- spec/scripts/spec.sh
+++ spec/scripts/spec.sh
@@ -50,6 +50,5 @@ IMAGE_NAME=${IMAGE_NAME} RUBY_VERSION=${VERSION} USER_ID="$(id -u):$(id -g)" \
   --rm ruby_rspec \
   /bin/bash -c "\
     gem install rake && \
-    gem install bundler && \
     bundle update && \
     timeout -s9 15m bin/run-tests ${TEST}"
