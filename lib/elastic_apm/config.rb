50c6f5143590ca36ddda79b69921f0c8fe978e94
Justin George <justin.george@elastic.co>
2022-06-02 06:36:25 -0700

    Initial pass at server version checking

--- lib/elastic_apm/config.rb
+++ lib/elastic_apm/config.rb
@@ -25,6 +25,7 @@ require 'elastic_apm/config/round_float'
 require 'elastic_apm/config/regexp_list'
 require 'elastic_apm/config/wildcard_pattern_list'
 require 'elastic_apm/deprecations'
+require 'elastic_apm/config/server_info'
 
 module ElasticAPM
   # @api private
