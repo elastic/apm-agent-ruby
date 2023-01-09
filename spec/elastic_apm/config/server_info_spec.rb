ee11fa92f7b43b301a8f6f966d462b3e4822a160
Emily S <emily.stolfo@elastic.co>
2023-01-09 11:09:51 +0100

    Don't send unsampled transactions to APM server >= 8.0 (#1341)
    * Improve server info code and add some tests
    
    * Update tests for new server version check
    
    * The server version is a string
    
    * Use the version 0 constant as the default

---
+++ spec/elastic_apm/config/server_info_spec.rb
@@ -0,0 +1,54 @@
+# Licensed to Elasticsearch B.V. under one or more contributor
+# license agreements. See the NOTICE file distributed with
+# this work for additional information regarding copyright
+# ownership. Elasticsearch B.V. licenses this file to you under
+# the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# frozen_string_literal: true
+
+require "spec_helper"
+
+module ElasticAPM
+  class Config
+    RSpec.describe ServerInfo do
+      subject { described_class.new(Config.new) }
+
+      context 'when the request is successful' do
+        context 'when the version is in the response' do
+          it 'sets the version' do
+            WebMock.stub_request(:get, %r{^http://localhost:8200/$}).
+              to_return(body: '{"version":"7.17.4"}')
+            expect(subject.version).to eq('7.17.4')
+          end
+        end
+
+        context 'when the version is not in the response' do
+          it 'sets the version to 0' do
+            WebMock.stub_request(:get, %r{^http://localhost:8200/$}).
+              to_return(body: nil)
+            expect(subject.version).to eq('0')
+          end
+        end
+      end
+
+      context 'when the request is not successful' do
+        it 'sets the version to 0' do
+          WebMock.stub_request(:get, %r{^http://localhost:8200/$}).
+            to_raise(StandardError)
+            expect(subject.version).to eq('0')
+        end
+      end
+    end
+  end
+end
