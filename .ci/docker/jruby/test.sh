50dfbead2d2817e986c9fd08f9e4bc022a66927a
Jan Calanog <nejcalanog@gmail.com>
2023-03-08 10:38:33 +0100

    Update .ci/docker/jruby/test.sh
    Co-authored-by: Victor Martinez <victormartinezrubio@gmail.com>

--- .ci/docker/jruby/test.sh
+++ .ci/docker/jruby/test.sh
@@ -17,7 +17,7 @@ fi
 
 test_name="Test Hello World"
 # random operation to verify that ruby evaluates the passed string correctly
-docker run -t --rm $image jruby -e "foo = 3 * 4; puts foo" | grep -q '13'
+docker run -t --rm $image jruby -e "foo = 3 * 4; puts foo" | grep -q '12'
 TEST_HELLO_WORLD_RESULT=$?
 if [[ $TEST_HELLO_WORLD_RESULT -eq 0 ]]; then
   printf '\t\t%-40s %s\n' "${test_name}" "PASSED"
