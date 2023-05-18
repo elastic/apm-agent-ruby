6ce467621109a60973cae1e56f4daf9aa4cd0e6b
Victor Martinez <VictorMartinezRubio@gmail.com>
2023-05-18 09:20:47 +0200

    ci: enable microbenchmark in GH actions

---
+++ .ci/scripts/bench.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+set -exo pipefail
+
+## Buildkite specific configuration
+if [ "$CI" == "true" ] ; then
+	# If HOME is not set then use the Buildkite workspace
+	# that's normally happening when running in the CI
+	# owned by Elastic.
+	if [ -z "$HOME" ] ; then
+		HOME=$BUILDKITE_BUILD_CHECKOUT_PATH
+		export HOME
+	fi
+
+	# required when running the benchmark
+	PATH=$PATH:$HOME/.local/bin
+	export PATH
+
+	echo 'Docker login is done in the Buildkite hooks'
+fi
+
+# It does not fail so it runs for every single version and then we report the error at the end.
+set +e
+status=0
+for VERSION in "ruby:3.1" "ruby:3.0" "ruby:2.7" "ruby:2.6" "ruby:9.2" ; do
+    ## Transform the versions like:
+    ## jruby:9.1 to jruby-9.1
+	OUTPUT_NAME=benchmark-$(echo "${VERSION//:/-}")
+
+	# APM_AGENT_GO* env variables are provided by the Buildkite hooks.
+	./spec/scripts/benchmarks.sh "${VERSION}" "${OUTPUT_NAME}" "$(pwd)"
+
+	if [ $? -gt 0 ] ; then
+		status=1
+	fi
+
+	# Then we ship the data using the helper
+	echo "TBCsendBenchmarks(file: \"{OUTPUT_NAME}.bulk\", index: \"benchmark-ruby\", archive: true)"
+done
+
+exit $status
--- spec/scripts/benchmarks.sh
+++ spec/scripts/benchmarks.sh
@@ -9,7 +9,8 @@ set -exo pipefail
 
 IMAGE_NAME=${1:?"missing RUBY IMAGE NAME"}
 VERSION=$(echo "${IMAGE_NAME}" | cut -d":" -f2)
-REFERENCE_REPO=${2}
+OUTPUT_NAME=${2:?"missing THE OUTPUT NAME"}
+REFERENCE_REPO=${3}
 
 if [ -z "${REFERENCE_REPO}" ] ; then
   REFERENCE_REPO_FLAG=""
@@ -17,13 +18,8 @@ else
   REFERENCE_REPO_FLAG="-v ${REFERENCE_REPO}:${REFERENCE_REPO}"
 fi
 
-## Transform the versions like:
-##  - docker.elastic.co/observability-ci/jruby:9.2-12-jdk to jruby-9.2-12-jdk
-##  - jruby:9.1 to jruby-9.1
-TRANSFORMED_VERSION=$(basename "${IMAGE_NAME}" | sed "s#:#-#g")
-
 local_vendor_path="$HOME/.cache/ruby-vendor"
-container_vendor_path="/tmp/vendor/${TRANSFORMED_VERSION/ruby-/}"
+container_vendor_path="/tmp/vendor/${OUTPUT_NAME/ruby-/}"
 
 mkdir -p "${local_vendor_path}"
 
@@ -48,6 +44,6 @@ IMAGE_NAME=${IMAGE_NAME} RUBY_VERSION=${VERSION} \
     gem install bundler
     bundle config --global set path '/tmp/.vendor'
     bundle install
-    bench/benchmark.rb 2> benchmark-${TRANSFORMED_VERSION}.error > benchmark-${TRANSFORMED_VERSION}.raw
-    bench/report.rb < benchmark-${TRANSFORMED_VERSION}.raw > benchmark-${TRANSFORMED_VERSION}.bulk"
+    bench/benchmark.rb 2> ${OUTPUT_NAME}.error > ${OUTPUT_NAME}.raw
+    bench/report.rb < ${OUTPUT_NAME}.raw > ${OUTPUT_NAME}.bulk"
 
