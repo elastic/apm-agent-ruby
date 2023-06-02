976a0635ae959392c857a5d0e7575c02bff1fc32
Victor Martinez <victormartinezrubio@gmail.com>
2023-06-02 10:53:09 +0200

    Update .ci/scripts/bench.sh

--- .ci/scripts/bench.sh
+++ .ci/scripts/bench.sh
@@ -25,19 +25,19 @@ status=0
 for VERSION in "ruby:3.1" "ruby:3.0" "ruby:2.7" "ruby:2.6" "jruby:9.2" ; do
     ## Transform the versions like:
     ## jruby:9.1 to jruby-9.1
-	OUTPUT_NAME=benchmark-$(echo "${VERSION//:/-}")
+  echo "--- Benchmark for :ruby: ${VERSION}"
+  OUTPUT_NAME=benchmark-$(echo "${VERSION//:/-}")
 
-	# TBC, maybe a timeout could help so it can run the other versions?
-	# APM_AGENT_GO* env variables are provided by the Buildkite hooks.
-	./spec/scripts/benchmarks.sh "${VERSION}" "${OUTPUT_NAME}" "$(pwd)"
+  # TBC, maybe a timeout could help so it can run the other versions?
+  ./spec/scripts/benchmarks.sh "${VERSION}" "${OUTPUT_NAME}" "$(pwd)"
 
-	# Gather error if any
-	if [ $? -gt 0 ] ; then
-		status=1
-	fi
+  # Gather error if any
+  if [ $? -gt 0 ] ; then
+    status=1
+  fi
 
-	# Then we ship the data using the helper
-	echo "TBC: sendBenchmarks(file: \"{OUTPUT_NAME}.bulk\", index: \"benchmark-ruby\", archive: true)"
+  # Then we ship the data using the helper
+  echo "TBC: sendBenchmarks(file: \"{OUTPUT_NAME}.bulk\", index: \"benchmark-ruby\", archive: true)"
 done
 
 # Report status
