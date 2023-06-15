284e46fb48ac4e96183e551e5291c702fd9d8da4
Adrien Mannocci <adrien.mannocci@elastic.co>
2023-06-15 12:17:05 +0200

    chore: debug raw and error benchmark
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>

--- .ci/scripts/bench.sh
+++ .ci/scripts/bench.sh
@@ -30,6 +30,8 @@ for VERSION in "ruby:3.1" "ruby:3.0" "ruby:2.7" "ruby:2.6" "jruby:9.2" ; do
 
   # TBC, maybe a timeout could help so it can run the other versions?
   ./spec/scripts/benchmarks.sh "${VERSION}" "${OUTPUT_NAME}" "$(pwd)"
+  cat "${BUILDKITE_BUILD_CHECKOUT_PATH}/.apm-agent-ruby/spec/${OUTPUT_NAME}.raw"
+  cat "${BUILDKITE_BUILD_CHECKOUT_PATH}/.apm-agent-ruby/spec/${OUTPUT_NAME}.error"
 
   # Gather error if any
   if [ $? -gt 0 ] ; then
