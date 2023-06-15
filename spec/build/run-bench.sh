138109265dcdb6b3048ad4765cac1c1f0014ca95
Adrien Mannocci <adrien.mannocci@elastic.co>
2023-06-15 18:46:27 +0200

    fix: correct reporting
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>

--- spec/build/run-bench.sh
+++ spec/build/run-bench.sh
@@ -7,5 +7,5 @@ export BUNDLE_PATH=/tmp/.vendor
 
 cp -rf ${VENDOR_PATH} /tmp/.vendor
 bundle install
-bench/benchmark.rb 2> /app/spec/${REPORT_OUTPUT_NAME}.error > /app/spec/${REPORT_OUTPUT_NAME}.raw
-bench/report.rb < /app/spec/${REPORT_OUTPUT_NAME}.raw > /app/spec/${REPORT_OUTPUT_NAME}.bulk"
+bench/benchmark.rb 2> "/app/spec/${REPORT_OUTPUT_NAME}.error" > "/app/spec/${REPORT_OUTPUT_NAME}.raw"
+bench/report.rb < "/app/spec/${REPORT_OUTPUT_NAME}.raw" > "/app/spec/${REPORT_OUTPUT_NAME}.bulk"
