ae3c68ecb7f8a8d92ac4c55d28698fa3fc930cfd
Adrien Mannocci <adrien.mannocci@elastic.co>
2023-06-20 17:05:55 +0200

    fix: correct bundler version
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>

--- spec/build/update.sh
+++ spec/build/update.sh
@@ -16,7 +16,7 @@ RUBY_VERSION=$(ruby -e 'print "#{ RUBY_VERSION }\n"')
 if [[ "${RUBY_VERSION}" == "2.5"* ]]; then
   gem i "rubygems-update:~>2.7" --no-document
   update_rubygems --no-document
-  gem i "bundler:~>2.3" --no-document
+  gem i "bundler:~>2.3.26" --no-document
 else
   gem update --system --no-document
   gem install bundler --no-document
