67d73cbe5a00b3a17dd434a34cc1c634b62e1776
Adrien Mannocci <adrien.mannocci@elastic.co>
2023-06-22 21:52:33 +0200

    ci: reset to root user
    Signed-off-by: Adrien Mannocci <adrien.mannocci@elastic.co>

--- spec/build/update.sh
+++ spec/build/update.sh
@@ -7,14 +7,15 @@ set -eo pipefail
 RUBY_VERSION=$(ruby -e 'print "#{ RUBY_VERSION }\n"')
 
 # Install specific dependencies for 2.5.x ruby versions
-if [[ "${RUBY_VERSION}" =~ ^2\.(4|5).+ ]]; then
-  gem i "rubygems-update:~>2.7" --no-document
-  update_rubygems --no-document
-  gem i "bundler:~>2.3.26" --no-document
-elif [[ "${FRAMEWORK}" =~ ^rails-4\.([0-9]) ]]; then
+if [[ "${FRAMEWORK}" =~ ^rails-4\.([0-9]) ]]; then
   gem i "rubygems-update:~>2.7" --no-document
   update_rubygems --no-document
   gem i "bundler:~>1.17.3" --no-document
+# Install specific dependencies for 2.4.x and 2.5.x ruby versions
+elif [[ "${RUBY_VERSION}" =~ ^2\.(4|5).+ ]]; then
+  gem i "rubygems-update:~>2.7" --no-document
+  update_rubygems --no-document
+  gem i "bundler:~>2.3.26" --no-document
 else
   gem update --system --no-document
   gem i bundler --no-document
